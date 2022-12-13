f9804ed4816445ad7e06578c317c661dae1c3e6f
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-13 21:20:03 +0300

    style: the style of the inputs are changed

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,10 @@
 function Input({ label, value, setValue }) {
   return (
     <div>
-      <label>{label}</label>
+      <label className="text-xl tracking-wide">{label}</label>
       <input
         type="text"
-        className=""
+        className="bg-[#3B3486] text-white w-32 mb-4 p-2 rounded-md focus:outline-none tracking-widest"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />

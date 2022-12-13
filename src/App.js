7446c6300b774c42e0067f6d0264a36e6793427e
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-13 21:04:42 +0300

    style: style was added to appjs

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ function App() {
   const [message, setMessage] = useState("");
 
   return (
-    <div className="container">
-      <h1>BMI Calculator</h1>
-      <div className="inputs">
-        <Input label="Weight(kg): " value={weight} setValue={setWeight} />
-        <Input label="Height(cm): " value={height} setValue={setHeight} />
-      </div>
-      <div className="button">
-        <Button />
-      </div>
-      <div className="result">
-        <Result />
+    <div className="App w-full h-screen flex flex-col items-center text-[#1B255A] text-center font-poppins">
+      <div className="container bg-white w-[31.25rem] h-[37.5rem] my-24 mx-auto rounded-xl">
+        <h1 className="text-center text-4xl mt-4 mb-8">BMI Calculator</h1>
+        <div className="inputs">
+          <Input label="Weight(kg): " value={weight} setValue={setWeight} />
+          <Input label="Height(cm): " value={height} setValue={setHeight} />
+        </div>
+        <div className="button">
+          <Button />
+        </div>
+        <div className="result">
+          <Result />
+        </div>
       </div>
     </div>
   );

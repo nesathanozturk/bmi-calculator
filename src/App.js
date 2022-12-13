2e96a9954e85527a60b10793ca9395155a30a864
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-13 20:23:39 +0300

    feat: created components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+import Result from "./components/Result/Result";
 import "./App.css";
 
 function App() {

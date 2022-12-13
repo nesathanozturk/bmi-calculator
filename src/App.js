49684b28d942ff0729f2325b2db269f8c023d25a
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-12-13 21:34:47 +0300

    style: edited appjs

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ function App() {
 
   return (
     <div className="App w-full h-screen flex flex-col items-center text-[#1B255A] text-center font-poppins">
-      <div className="container bg-white w-[28rem] h-[37.5rem] my-24 mx-auto rounded-xl">
-        <h1 className="text-center text-4xl mt-4 mb-8">BMI Calculator</h1>
-        <div className="inputs">
+      <div className="container bg-white w-[25rem] h-[37.5rem] my-24 mx-auto rounded-xl">
+        <h1 className="text-4xl mt-4">BMI Calculator</h1>
+        <div className="inputs mt-8 mb-2">
           <Input label="Weight(kg): " value={weight} setValue={setWeight} />
           <Input label="Height(cm): " value={height} setValue={setHeight} />
         </div>
-        <div className="button">
+        <div className="button mb-8">
           <Button />
         </div>
-        <div className="result">
+        <div className="result w-48 p-2 mx-auto border-2 border-[#3B3486] text-left">
           <Result />
         </div>
       </div>

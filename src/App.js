1104217b0e8df0a063c8fd4437ba718cb195f4a4
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2023-01-31 14:08:43 +0300

    style: edited bmi card

--- src/App.js
+++ src/App.js
@@ -32,25 +32,25 @@ function App() {
   };
 
   return (
-    <div className="App w-full h-screen flex flex-col items-center text-[#1B255A] text-center font-poppins">
-      <div className="container bg-black w-[22rem] h-[33rem] my-24 mx-auto rounded-xl shadow-2xl">
+    <div className="App w-full h-screen m-0 p-0 box-border flex flex-col items-center text-[#1B255A] text-center font-poppins">
+      <div className="container bg-white w-[22rem] h-[33rem] p-4 my-24 mx-auto rounded-xl shadow-2xl">
         <h1 className="text-4xl mt-4 mb-8">BMI Calculator</h1>
         <div className="result w-52 mt-4 p-2 mx-auto border-2 border-[#3B3486]">
           <Result bmi={bmi} message={message} />
         </div>
-        <div className="inputs mt-8 mb-2">
+        <div className="mt-8 mb-2">
           <Input label="Height(cm): " value={height} setValue={setHeight} />
           <Input label="Weight(kg): " value={weight} setValue={setWeight} />
         </div>
-        <div className="button mb-2">
+        <div className="mb-2">
           <Button calcBmi={calcBmi} />
         </div>
-        <div className="reset-button">
+        <div>
           <ResetButton />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
